2d0df28116d66f8d0f1ae7f8b7c9d75cbe6f06b6
shuzihao <593086592@qq.com>
2016-08-12 16:15:04 +0800

    111

---
+++ src/main/java/com/cvr/Application.java
@@ -0,0 +1,15 @@
+package com.cvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by shuzihao on 2016/8/2.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+
+        SpringApplication.run(new Object[]{Application.class},args);
+    }
+}
